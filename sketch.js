f8b8b2e04e032065e6e5de3405141eaea1e3999f
sjsu-koby <koby.nguyen@sjsu.edu>
2022-05-15 23:20:29 -0700

    editted sketch and imported new files

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
+var stick, platform, ground;
+
+var GRAVITY = 1;
+var JUMP = 15
+
 function setup() {
   // put setup code here
   createCanvas(1080,1350);
+
+  stick = createSprite(200,200);
+  stick = addAnimation('assets/stick.png');
+
+  platform = createSprite(200,300);
+  platform.addImage('assets/platform.png');
 }
 
 function draw() {
   // put drawing code here
   background(220);
+
+  textAlign(CENTER);
+  text('Reach the top to win', width / 2, 20);
 }
