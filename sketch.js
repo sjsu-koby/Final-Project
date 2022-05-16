60d2f22e8da5fa246261c7e09f1edadeba5401c2
sjsu-koby <koby.nguyen@sjsu.edu>
2022-05-16 00:59:21 -0700

    Adjusted index and added more elements to sketch

--- sketch.js
+++ sketch.js
@@ -1,23 +1,68 @@
 var stick, platform, ground;
+var platformImg;
+var gameOver;
+var gameState = 'title';
 
 var GRAVITY = 1;
 var JUMP = 15
 
 function setup() {
-  // put setup code here
-  createCanvas(1080,1350);
 
+  createCanvas(700,900);
+  platformImg = loadImage('assets/platform.png');
   stick = createSprite(200,200);
-  stick = addAnimation('assets/stick.png');
+  stick = loadImage('assets/stick.png');
 
   platform = createSprite(200,300);
-  platform.addImage('assets/platform.png');
+  platform.addImage(platformImg);
 }
 
 function draw() {
-  // put drawing code here
-  background(220);
 
+  switch (gameState) {
+    case 'title':
+      titleScreen();
+      break;
+    case 'lvl1':
+      gameStage1();
+      break;
+    case 'gameOver':
+      gameOver();
+      break;
+  }
+  if (stick.collide(platform)) {
+    dog.velocity.y = 0;
+  }
+}
+
+function keyReleased() {
+  if (gameState === 'title' || gameState === 'gameover') {
+  if (key === ' ' || key === ' ') {
+    gameState = 'lvl1';
+    background(220);
+    }
+  }
+}
+
+function titleScreen() {
+  background(220);
+  textSize(70);
   textAlign(CENTER);
-  text('Reach the top to win', width / 2, 20);
+  text('The Water is Lava', width / 2, height * .25);
+  textSize(30);
+  fill(100);
+  text('(Press "SPACE" To Play)', width / 2, height * .30);
+}
+
+function gameStage1() {
+
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    if (gameOver)
+      newGame();
+    stick.velocity.y = JUMP;
+  }
+
 }
