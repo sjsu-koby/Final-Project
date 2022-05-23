7993723ffb9e77a0da99325c84bce5a7cfd5903c
sjsu-koby <koby.nguyen@sjsu.edu>
2022-05-23 04:30:03 -0700

    Made moving platforms and changed assets

---
+++ bird.js
@@ -0,0 +1,39 @@
+class Bird {
+  constructor(x, y, xSpeed, ySpeed) {
+    this.x = x;
+    this.y = y;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+  }
+
+  move() {
+  this.x += this.xSpeed;
+  if (this.x < 0 || this.x > width) {
+    this.xSpeed *= -1;
+  }
+
+  this.y += this.ySpeed;
+  if (this.y < 0 || this.y > height) {
+    this.ySpeed *= -1;
+  }
+}
+//  move() {
+//    if (this.x > width / 2.4) {
+//      changeDirection = true
+//    } else if (this.x <= 0) {
+//      changeDirection = false
+//    }
+//    if (this.x >= 0 && changeDirection == false) {
+//      this.x = this.x + 1
+//    } else if (changeDirection == true) {
+//      this.x = this.xPos - 1
+//    }
+
+//  }
+  display() {
+    fill(0);
+    triangle(this.x, this.y, this.x + 2, this.y + 8, this.x + 5, this.y - 10)
+    ellipse(this.x, this.y, 12, 3)
+  }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
 var GRAVITY = 0.4;
-var JUMP = -11;
+var JUMP = -12;
 var GROUND_Y = 450;
 var MIN_OPENING = 300;
-var stick, ground, ground1, water;
+var stick, ground, ground1, water, platforms, x;
 var gameOver;
-var stickImg, waterImg, groundImg, ground1Img;
+var stickImg, waterImg, groundImg, ground1Img, birdImg;
+var platform1, platform2, platform3;
 var gameState = 'title';
-var floor1;
-var floor2;
-var floor3;
+var birds = [];
 var changeDirection;
 var jumpSound;
 var music;
@@ -19,44 +18,66 @@ function preload() {
   jumpSound = loadSound('assets/Mario-jump-sound.mp3')
   stickLeft = loadAnimation('assets/stick.png');
   stickRight = loadAnimation('assets/stick.png');
+  platformImg = loadImage('assets/platform.png');
 }
 
 function setup() {
   createCanvas(700, 900);
+  frameRate(30);
   music.loop();
+  changeDirection = false;
+  for (let i = 0; i <= 5; i++) {
+   birds[i] = new Bird(
+   random(width),
+   random(height),
+   random(-3, 3),
+   random(-3, 3)
+ );
+  }
+
   slider = createSlider(0, 1, 0.5, 0.01);
   slider.position(CENTER, height * 0.01);
-  // 0.1 is highest
-  floor1 = new Floor(0, height * .30);
-  floor2 = new Floor(150, height * .25);
-  floor3 = new Floor(100, height * .1);
+
   stickImg = loadImage('assets/stick.png');
-  waterImg = loadImage('assets/water.png');
+  waterImg = loadImage('assets/water1.png');
   groundImg = loadImage('assets/platform.png');
   ground1Img = loadImage('assets/platform.png');
 
-  stick = createSprite(10, 675);
+  stick = createSprite(350, 675);
   stick.velocity.x = 0;
   stick.setCollider('rectangle', 0, 0, 40, 80);
   stick.addImage(stickImg);
 
-  ground = createSprite(10, 680); //image 360x20
+  ground = createSprite(350, 680); //image 360x20
   ground.setCollider('rectangle', 0, 0, 120, 21);
   ground.addImage(groundImg);
 
-  ground1 = createSprite(10, 80); //image 360x20
+  platform1 = createSprite(random(1,100), 540);
+  platform1.setCollider('rectangle', 0, 0, 120, 21);
+  platform1.addImage(groundImg);
+
+  platform2 = createSprite(random(100,200), 400);
+  platform2.setCollider('rectangle', 0, 0, 120, 21);
+  platform2.addImage(groundImg);
+
+  platform3 = createSprite(random(200,300), 250);
+  platform3.setCollider('rectangle', 0, 0, 120, 21);
+  platform3.addImage(groundImg);
+
+
+  ground1 = createSprite(350, 80); //image 360x20
   ground1.setCollider('rectangle', 0, 0, 120, 21);
   ground1.addImage(groundImg);
 
   water = createSprite(0, 1200);
-  water.addImage(waterImg);
+  water.addAnimation('wave', 'assets/water1.png', 'assets/water2.png');
 
-  //floor1 = createSprite(floor1);
-  Floor = new Group();
+  Bird = new Group
   gameOver = true;
   updateSprites(false);
 
-  camera.position.y = height / 2;
+  //camera.position.y = height / 2;
+
 }
 
 function draw() {
@@ -76,9 +97,19 @@ function draw() {
       break;
   }
   //stick.collide(ground);
-  //stick.collide(floor1);
-  if (stick.collide(ground, Floor)) {
+  //stick.collide(bird1);
+
+  if (stick.collide(ground)) {
     stick.velocity.y = 0;
+  }
+  if (stick.collide(platform1)) {
+   stick.velocity.y = 0;
+  }
+  if (stick.collide(platform2)) {
+   stick.velocity.y = 0;
+  }
+  if (stick.collide(platform3)) {
+   stick.velocity.y = 0;
   }
 }
 
@@ -94,25 +125,57 @@ function titleScreen() {
   background(220);
   textSize(70);
   textAlign(CENTER);
-  text('The Water is Lava', width * .01, height * .4);
+  text('The Water is Lava', width * .5, height * .4);
   textSize(30);
   fill(100);
-  text('(Press "SPACE" to start)', width * .01, height * .5);
+  text('(Press "SPACE" to start)', width * .5, height * .5);
   textSize(40);
   fill(0);
-  text('How to Play', width * .01, height * .65);
+  text('How to Play', width * .5, height * .65);
   textSize(20);
   fill(100);
-  text('Reach the top to win!', width * .01, height * .7)
-  text('Controls: "SPACE" to jump. "LEFT/RIGHT ARROWKEYS" to move.', width * .01, height * .75)
+  text('Reach the top to win!', width * .5, height * .7)
+  text('Controls: "SPACE" to jump. "LEFT/RIGHT ARROWKEYS" to move.', width * .5, height * .75)
+  text('Jump once or you are cheating 😡', width * .5, height * .80)
 }
 
 function gameStage1() {
-
+  //console.log('Cursor at: '+mouseX+', '+mouseY);
+  push();
+  if(platform1.position.x > width){
+		changeDirection=true}
+	else if (platform1.position.x <= 0){
+		changeDirection = false}
+	if (platform1.position.x >= 0 && changeDirection == false){
+		platform1.position.x = platform1.position.x + 1}
+	else if(changeDirection == true){
+		platform1.position.x = platform1.position.x - 1}
+  pop();
+  push();
+  if(platform2.position.x > width){
+    changeDirection=true}
+  else if (platform2.position.x <= 0){
+    changeDirection = false}
+  if (platform2.position.x >= 0 && changeDirection == false){
+    platform2.position.x = platform2.position.x + 1}
+  else if(changeDirection == true){
+    platform2.position.x = platform2.position.x - 1}
+  pop();
+  push();
+  if(platform3.position.x > width){
+    changeDirection=true}
+  else if (platform3.position.x <= 0){
+    changeDirection = false}
+  if (platform3.position.x >= 0 && changeDirection == false){
+    platform3.position.x = platform3.position.x + 1}
+  else if(changeDirection == true){
+    platform3.position.x = platform3.position.x - 1}
+  pop();
   stick.velocity.x = 0;
   if (keyIsDown(LEFT_ARROW)) stick.velocity.x = -5;
   if (keyIsDown(RIGHT_ARROW)) stick.velocity.x = 5;
   if (gameOver && keyWentDown(' '))
+
     newGame();
 
   if (!gameOver) {
@@ -132,15 +195,15 @@ function gameStage1() {
       win();
   }
 
-  camera.off();
+  // camera.off();
   background(220);;
-  floor1.display();
-  floor2.display();
-  floor3.display();
-  floor1.move();
-  floor2.move();
-  floor3.move();
-  camera.on();
+  for (i = 0; i < birds.length; i++) {
+  birds[i].move();
+  birds[i].display();
+
+}
+
+  //camera.on();
 
   drawSprites();
 }
@@ -150,10 +213,10 @@ function newGame() {
 
   gameOver = false;
   updateSprites(true);
-  stick.position.x = 10;
+  stick.position.x = 350;
   stick.position.y = 675;
   stick.velocity.y = 0;
-  ground.position.x = 10;
+  ground.position.x = 350;
   ground.position.y = 680;
 }
 
@@ -170,10 +233,11 @@ function youDied() {
   background(220);
   textSize(70);
   textAlign(CENTER);
-  text('Game Over', width * .01, height * .4);
+  fill(0);
+  text('Game Over', width * .5, height * .4);
   textSize(30);
   fill(100);
-  text('(Press "SPACE" To Try Again)', width * .01, height * .5);
+  text('(Press "SPACE" To Try Again)', width * .5, height * .5);
 }
 
 function win() {
@@ -184,19 +248,20 @@ function win() {
   }
 }
 
-  function victory() {
+function victory() {
     gameOver = true;
     updateSprites(false);
     background(220);
     textSize(70);
     textAlign(CENTER);
-    text('You Survived!', width * .01, height * .4);
+    fill(0);
+    text('You Survived!', width * .5, height * .4);
     textSize(30);
     fill(100);
-    text('(Press "SPACE" To Play Again)', width * .01, height * .5);
+    text('(Press "SPACE" To Play Again)', width * .5, height * .5);
   }
 
-  function keyPressed() {
+function keyPressed() {
     if (keyCode === 32) {
       jumpSound.play();
       jumpSound.setVolume(0.1);
